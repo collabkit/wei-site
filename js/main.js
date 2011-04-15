b4ae2b1145e3ba2a6c3dd4e5f1c9893001bd28c6
Trevor Parscal <trevorparscal@gmail.com>
2011-04-15 00:48:22 -0700

    Moved JavaScript and CSS resources to js and css folders. Moved email image to the images folder.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready( function() {
+	var $window = $(window);
+	var elements = {
+		'logo': { '$': $( '#logo' ), 'fixed': null, 'limit': 0, 'hidden': false },
+		'menu': { '$': $( '#menu' ), 'fixed': null, 'limit': 0, 'hide': 'logo' }
+	};
+	function reflow() {
+		for ( key in elements ) {
+			e = elements[key];
+			if ( e.hidden ) {
+				continue;
+			}
+			if ( e.fixed === null ) {
+				e.fixed = $window.scrollTop() >= e.$.offset().top;
+				if ( e.fixed ) {
+					e.$.addClass( 'fixed' );
+					e.limit = e.$.outerHeight();
+				}
+			} else if ( !e.fixed && $window.scrollTop() >= e.$.offset().top ) {
+				e.$.addClass( 'fixed' );
+				e.fixed = true;
+				e.limit = $window.scrollTop();
+			} else if ( e.fixed && $window.scrollTop() < e.limit - 1 ) {
+				e.$.removeClass( 'fixed' );
+				e.fixed = false;
+			}
+			if ( e.hide in elements ) {
+				elements[e.hide].$[e.fixed ? 'hide' : 'show']();
+				elements[e.hide].hidden = e.fixed;
+			}
+		}
+	}
+	$window.bind( 'load scroll mousewheel', reflow );
+	$( 'a[href^=#]' ).click( function() {
+		setTimeout( reflow, 0 );
+	} );
+	$( '#posts' ).PaRSS(
+		'http://sheandhimcook.blogspot.com/feeds/posts/default', 3, 'M jS g:i a', true
+	);
+} );
